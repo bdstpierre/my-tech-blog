a712aaf87ec32a5a49e15aa02e828ea8d9f8d3a0
bdstpierre <barry@stpierre.com>
2021-07-22 23:33:59 -0400

    Committing homeRoutes, believe homeRoutes is complete (if not correct)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {Post, User } = require('../models');
+const {Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -17,33 +17,47 @@ router.get('/', async (req, res) => {
         // Serialize data so the template can read it
         const posts = postData.map((post) => post.get({ plain: true }));
 
-        // Pass serialized data and session flag into template
+        // Pass serialized data and session flag into template (don't need to be logged in for homepage)
         res.render('homepage', {
-            posts,
-            logged_in: req.session.loggen_in
+            posts
         });
+
+        // Output for postman testing
+        // res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-router.get('/post/:id', async (req, res) => {
+router.get('/post/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
             include: [
                 {
                     model: User,
-                    attributes: ['volunteer'],
+                    attributes: {exclude: ['password']},
                 },
+                {
+                    model: Comment,
+                    attributes: [
+                        comment,
+                        commenter,
+                        date,
+                    ]
+                }
             ],
         });
 
         const post = postData.get({ plain: true });
 
+        // Pass serialized data and session flag into template (must be logged in)
         res.render('post', {
             ...post,
             logged_in: req.session.logged_in
         });
+
+        // Output for postman testing
+        // res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
     };
@@ -62,7 +76,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
 
         res.render('dashboard', {
             ...user,
-            logged_in: true
+            logged_in: req.session.logged_in
         });
 
     } catch (err) {
@@ -73,11 +87,21 @@ router.get('/dashboard', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-        res.redirect('/dashboard');
+        res.redirect('/');
         return;
     }
 
     res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
 });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'author',
@@ -8,6 +9,24 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
     foreignKey: 'author'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'commenter',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'commenter'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
 });
 
 module.exports = { User, Post };
