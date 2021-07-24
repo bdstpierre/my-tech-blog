78fee90b79b695e4f46437ebc18800ed3f3d9a29
bdstpierre <barry@stpierre.com>
2021-07-24 16:42:41 -0400

    post routes complete with post, update and delete.  Currently all responses are to comsole, not render

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -9,10 +9,31 @@ router.post('/', withAuth, async (req, res) => {
             user_id: req.session.user_id,
         });
 
-        req.status(200).json(newPost);
+        res.status(200).json(newPost);
+
     } catch (err) {
         res.status(400).json(err);
     };
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatedPost = await Post.update({
+            ...req.body
+        },
+        {
+            where: {
+                id: req.params.id,
+                // user_id: req.params.user_id
+            }
+    });
+
+        res.status(200).json(updatedPost);
+
+    } catch (err) {
+        res.status(500).json(err);
+    };
+
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
